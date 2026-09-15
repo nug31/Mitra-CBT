f6dfa382c969c85ab80dca70e60dd65171badf7d
nug31 <nugrohodc06@gmail.com>
2026-09-15 13:48:41 +0700

    fix: use type-only import for Plugin in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, Plugin } from 'vite'
+import { defineConfig, type Plugin } from 'vite'
 import fs from 'fs'
 import path from 'path'
 
